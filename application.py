30441d8c9400c249ec8d6fca7b9bf032f5054456
Ruslan Gatin <ru100fa@yandex.ru>
2022-02-24 02:08:17 +0500

    Merge

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 print("Some string")
 print("Other string")
-<<<<<<< HEAD
+
 print("Other string")
 print("Other string")
 print("Other string")
 print("Other string")
-=======
 
 
 def new_func():
@@ -16,4 +15,3 @@ new_func()
 
 new_var = print("Other string")
 new_var
->>>>>>> bugfix
