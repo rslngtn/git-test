afb62bbdae2910ec5f10d6eaaa55bff333b7b7ac
Ruslan Gatin <ru100fa@yandex.ru>
2022-02-24 18:48:58 +0500

    add

--- application.py
+++ application.py
@@ -5,13 +5,3 @@ print("Other string")
 print("Other string")
 print("Other string")
 print("Other string")
-
-
-def new_func():
-    print("Other string")
-
-
-new_func()
-
-new_var = print("Other string")
-new_var
